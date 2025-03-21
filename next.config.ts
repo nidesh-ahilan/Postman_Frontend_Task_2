4bdbed040da36f4693ea52ebcc371595c527e56a
nidesh-ahilan <nideshahilan27@gmail.com>
2025-03-21 21:08:16 +0530

    commit 2

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+        output: "export",        // Enable static export
+        basePath: "/Postman_Frontend_Task_2", // Set the base path for GitHub Pages
+        trailingSlash: true,      // Add trailing slashes to URLs
 };
+
 
 export default nextConfig;
